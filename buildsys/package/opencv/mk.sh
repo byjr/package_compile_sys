fee5006cfa68abb0416f1adbc3b478a3058743b8
jonny <byjr@outlook.com>
2020-10-19 01:29:20 -0400

    fix opencv

--- buildsys/package/opencv/mk.sh
+++ buildsys/package/opencv/mk.sh
@@ -7,6 +7,7 @@ pkg_source_url=https://github.com/opencv/opencv/archive
 source $TOP_DIR/include/gloable_utils.sh
 source $TOP_DIR/include/make_com_var.sh
 pkg_source_uri=$pkg_source_url/$pkg_ver.$cur_archive_type
+
 function pkg_sync(){
 	[ -e $dst_path/$sync_sfile ] && return 0
 	func_info $0 $LINENO $FUNCNAME
@@ -18,7 +19,7 @@ function pkg_sync(){
 }
 function pkg_config(){
 	[ -e $dst_path/$config_sfile ] && return 0
-	func_info $0 $LINENO $FUNCNAME
+	func_info $0 $LINENO $FUNCNAMEs 
 	install_path $dst_path/build
 	cd $dst_path/build && \
 		cmake $dst_path/src \
@@ -27,5 +28,12 @@ function pkg_config(){
 			-DCMAKE_LIBRARY_PATH=$PRO_STAG_USR_PATH/lib
 		res_info $? "[$0:$LINENO]:$FUNCNAME"
 }
-
+function pkg_build(){
+	[ -e $dst_path/$build_sfile ] && return 0
+	func_info $0 $LINENO $FUNCNAME
+	cd $dst_path/build && \
+	make -j $PROCESS_NUM && \
+	make install
+	res_info $? "[$0:$LINENO]:$FUNCNAME"
+}
 source $TOP_DIR/include/make_com_script.sh
