c8819d9cbda8898bcf352831b239d694ec130ac9
liuzhuang <zhuang.liu@3nod.com.cn>
2020-12-07 11:12:44 +0800

    add logctrl

--- app_sources/DlnaServer/src/main.cpp
+++ app_sources/DlnaServer/src/main.cpp
@@ -3,8 +3,22 @@
 #include "Dlna/DlnaDmrSdk.h"
 #include "Dlna/DlnaDmrOutputFfmpeg.h"
 #include "DlnaPlayerImpl.h"
+#include <getopt.h>
 using namespace duerOSDcsApp;
 int main(int argc, char** argv) {
+	int opt = 0;
+	while ((opt = getopt_long_only(argc, argv, "l:p:h", NULL, NULL)) != -1) {
+		switch (opt) {
+		case 'l':
+			lzUtils_logInit(optarg, NULL);
+			break;
+		case 'p':
+			lzUtils_logInit(NULL, optarg);
+			break;
+		default: /* '?' */
+			s_err("no para!");
+		}
+	}	
 	auto dlnaPlayerImpl = mediaPlayer::DlnaPlayerImpl::create();
 	if(!dlnaPlayerImpl){
 		s_err("DlnaPlayerImpl create failed!");
